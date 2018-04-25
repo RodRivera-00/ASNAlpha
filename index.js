4e75a574cd22ee227756f690a9bed52bd39db97a
yeoninthegreat <yeoninthgreat@gmail.com>
2018-04-25 23:58:04 +0800

    Added initial files for node. Fixed plyr.io integration.

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('express-session')
+var url = require('url');
+var app = express()
+app.use(express.urlencoded());
+app.use(express.static('v1'))
+app.use(session({
+    secret: 'animeninjastream',
+}));
+app.set('view engine', 'ejs');
+var data = {
+    session: null,
+    episode: 0,
+    animeid: 0
+}
+app.get("/:episodenum", function (req, res) {
+    data.session = req.session
+    data.episode = req.params.episodenum
+    res.render("watch", data)
+})
+app.listen(80, () => console.log('Started at port 80'))
